4f70e7c7e809acd31adc6c69f9e4e8d7a7bb1794
Vasya Redneck <vasya.redneck@gmail.com>
2016-07-05 16:06:57 +0300

    clean

--- common/clean-corpus.R
+++ common/clean-corpus.R
@@ -1,34 +1,32 @@
 (function() {
-    print(getwd())
+    library("tm", quietly=T)
     
-    # http://www.freewebheaders.com/full-list-of-bad-words-banned-by-google/
-    con <- file("./bad-words-dict/bad-words-banned-by-google.txt", "r")
-    swear.words <- readLines(con, warn=F)
-    close(con)
-    # http://www.bannedwordlist.com/
-    con <- file("./bad-words-dict/swearWords.txt", "r")
-    swear.words <- c(swear.words, readLines(con, warn=F))
-    close(con)
+    source("./read-sentences.R", chdir=T)
     
-    CleanCorpus <<- function(corpus, remove.stop.words=T) {
-        getTransformations()
-        result <- tm_map(corpus, content_transformer(tolower))
-        result <- tm_map(result, removePunctuation)
-        result <- tm_map(result, removeNumbers)
-        if (remove.stop.words) {
-            # Remove english stop words (like the, a, an, etc)
-            result <- tm_map(result, removeWords, stopwords("english"))
-        }
-        # Remove profanity words
-        result <- tm_map(result, removeWords, swear.words)
+    swear.words <- c(
+            # http://www.freewebheaders.com/full-list-of-bad-words-banned-by-google/
+            readLines("./bad-words-dict/bad-words-banned-by-google.txt", warn=F, skipNul=T),
+            # http://www.bannedwordlist.com/
+            readLines("./bad-words-dict/swearWords.txt", warn=F, skipNul=T))
+
+    getTransformations()
+
+    CleanCorpus <<- function(corpus) {
+        RemoveStopWords <- function(x) removeWords(x, stopwords("english"))
+        RemoveSwearWords <- function(x) removeWords(x, swear.words)
         RemoveByPattern <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
-        # Remove URLs
-        result <- tm_map(result, RemoveByPattern, "(ht|f)tps?://([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([/\\w \\.-]*)*/?")
-        # Remove emails
-        result <- tm_map(result, RemoveByPattern, "([a-z0-9_\\.-]+)@([a-z0-9_\\.-]+)\\.([a-z\\.]{2,6})")
-        # Remove accounts
-        result <- tm_map(result, RemoveByPattern, "@[^\\s]+")
-        result <- tm_map(result, stripWhitespace)
-        return(result)
+        RemoveURLs <- function(x) RemoveByPattern(x, "(ht|f)tps?://([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([/\\w \\.-]*)*/?")
+        RemoveEmails <- function(x) RemoveByPattern(x, "([a-z0-9_\\.-]+)@([a-z0-9_\\.-]+)\\.([a-z\\.]{2,6})")
+        RemoveAccounts <- function(x) RemoveByPattern(x, "@[^\\s]+")
+        transformations <- list(content_transformer(tolower),
+                                removePunctuation,
+                                removeNumbers,
+                                RemoveStopWords,
+                                RemoveSwearWords,
+                                RemoveURLs,
+                                RemoveEmails,
+                                RemoveAccounts,
+                                stripWhitespace)
+        return(tm_map(corpus, FUN=tm_reduce, tmFuns=transformations))
     }
 })()
